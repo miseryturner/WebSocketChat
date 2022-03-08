7d2166a045b12e497a465eab60887f7d4a183df8
miseryturner <mmizari2019@mail.ru>
2022-03-09 02:36:12 +0500

    more fixes

--- server/database/messanger.sql
+++ server/database/messanger.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Мар 06 2022 г., 01:21
+-- Время создания: Мар 09 2022 г., 00:35
 -- Версия сервера: 8.0.24
 -- Версия PHP: 8.0.14
 
@@ -24,15 +24,64 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `chats`
+--
+
+CREATE TABLE `chats` (
+  `id` int NOT NULL,
+  `date_of_creation` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `first_id` int NOT NULL,
+  `second_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+
+--
+-- Дамп данных таблицы `chats`
+--
+
+INSERT INTO `chats` (`id`, `date_of_creation`, `first_id`, `second_id`) VALUES
+(4, '', 58, 59),
+(5, '', 60, 59),
+(7, 'Tue Mar 08 2022 03:02:10 GMT+0500 (Екатеринбург, стандартное время)', 59, 61);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` int NOT NULL,
+  `text` varchar(10000) COLLATE utf8_unicode_ci NOT NULL,
+  `from_user_id` int NOT NULL,
+  `chat_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+
+--
+-- Дамп данных таблицы `messages`
+--
+
+INSERT INTO `messages` (`id`, `text`, `from_user_id`, `chat_id`) VALUES
+(238, 'ываыва', 59, 5),
+(239, 'ываыва', 59, 5),
+(240, '', 59, 5),
+(241, '', 59, 5),
+(242, 'test', 59, 5),
+(243, 'ПРИВЕТ', 59, 5),
+(244, 'G', 59, 7),
+(245, 'GG', 61, 7);
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `users`
 --
 
 CREATE TABLE `users` (
-                         `id` int NOT NULL,
-                         `login` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-                         `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-                         `profile_picture` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-                         `phone` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+  `id` int NOT NULL,
+  `login` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `profile_picture` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `phone` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
 
 --
@@ -40,19 +89,36 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `login`, `password`, `profile_picture`, `phone`) VALUES
-                                                                                (43, '123123123', 'e6db1baa29d3df1eb307ff6a12c778da', '', '123123'),
-                                                                                (45, '123123123', 'e6db1baa29d3df1eb307ff6a12c778da', '', '1231232'),
-                                                                                (46, 'test', '098f6bcd4621d373cade4e832627b4f6', '', '898227');
+(58, 'test', '4297f44b13955235245b2497399d7a93', '', '+7 (123) 123-12-31'),
+(59, 'Максим Ромазанов', '7fded2590fd9fe9fd2e7b9cf86910dfc', '', '+7 (982) 275-61-33'),
+(60, 'NewUser', '4297f44b13955235245b2497399d7a93', '', '+7 (800) 555-35-35'),
+(61, 'vladik', '4297f44b13955235245b2497399d7a93', '', '+7 (111) 111-11-11');
 
 --
 -- Индексы сохранённых таблиц
 --
 
 --
+-- Индексы таблицы `chats`
+--
+ALTER TABLE `chats`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `first_id` (`first_id`),
+  ADD KEY `second_id` (`second_id`);
+
+--
+-- Индексы таблицы `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `from_user_id` (`from_user_id`),
+  ADD KEY `chat_id` (`chat_id`);
+
+--
 -- Индексы таблицы `users`
 --
 ALTER TABLE `users`
-    ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `phone` (`phone`);
 
 --
@@ -60,10 +126,40 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT для таблицы `chats`
+--
+ALTER TABLE `chats`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT для таблицы `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=246;
+
+--
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-    MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `chats`
+--
+ALTER TABLE `chats`
+  ADD CONSTRAINT `chats_ibfk_1` FOREIGN KEY (`first_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `chats_ibfk_2` FOREIGN KEY (`second_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `messages`
+--
+ALTER TABLE `messages`
+  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`from_user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`chat_id`) REFERENCES `chats` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
